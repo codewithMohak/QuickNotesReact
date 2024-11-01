54c9d5a06705c797da1795190bc78ff6189c651f
Mohak Agarwal <155367989+codewithMohak@users.noreply.github.com>
2024-11-01 21:14:08 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,36 +1,6 @@
-import React from "react";
-import seriesData from "./api/seriesData.json";
+import NetflixSeries from "./components/NetflixSeries.jsx";
 
-const App = () => {
-  return (
-    <ul>
-      {seriesData.map((curElem) => {
-        return (
-          <li key={curElem.id}>
-            <div>
-              <div>
-                <img
-                  src={curElem.img_url}
-                  alt={curElem.name}
-                  width="40%"
-                  height="40%"
-                />
-              </div>
-              <h2>Name: {curElem.name}</h2>
-              <h3>Rating: {curElem.rating}</h3>
-              <p>Summary: {curElem.summary}</p>
-              <p>Genre: {curElem.genre}</p>
-              <p>Cast: {curElem.cast}</p>
-              <a href={curElem.watch_url} target="_blank">
-                <button>Watch Now</button>
-              </a>
-            </div>
-          </li>
-        );
-      })}
-      ;
-    </ul>
-  );
+ const App = () => {
+  return <NetflixSeries/>
 };
-
 export default App;
